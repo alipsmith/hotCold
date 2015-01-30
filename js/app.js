63576243bd877b269b704c651a905a96ba5a6068
Ali Smith <alipsmith@me.com>
2015-01-30 11:43:34 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,97 @@
+
+$(document).ready(function(){
+	
+	/*--- Display information modal box ---*/
+  	$(".what").click(function(){
+    	$(".overlay").fadeIn(1000);
+
+  	});
+
+  	/*--- Hide information modal box ---*/
+  	$("a.close").click(function(){
+  		$(".overlay").fadeOut(1000);
+  	});
+
+//variables
+var randomNumVar;
+var userGuess;
+
+//on load, automatically generates random number
+randomNumFunc();
+
+// user enters number and clicks guess-button
+$('form').on('submit', function() {
+	event.preventDefault();
+	var userGuess = $('input#userGuess').val();
+	if ((userGuess > 0) && (userGuess < 101)) {
+		playGame();
+		clearInputField();
+	}else{
+		$('h2#feedback').html('Between 1 and 100!');
+		clearInputField();
+	}
+});
+
+//game play after user clicks guess-button
+function playGame() {
+	var userGuess = $('input#userGuess').val();
+	$('ul#guessList').append('<li>' + userGuess + '</li>');
+	guessCounter();
+	resetFeedback();
+	hotCold();	
+}
+
+//hot and cold feedback
+function hotCold() {
+	if (userGuess === randomNumVar) {
+		$('h2#feedback').html('Bingo!');	
+	}
+	else if (userGuess >= (randomNumVar+50)) {
+		$('h2#feedback').html('You\'re in the Dead Zone!');	
+	}
+}
+
+// new game -->
+$('.new').on('click', function() {
+	clearInputField();
+	resetFeedback();
+	$('span#count').html('0');
+	$('ul#guessList').empty();
+});
+
+// returns random integer //
+function randomNumFunc() {
+var randomNumVar = Math.floor((Math.random() * 100) + 1);
+console.log('your random number is ' + randomNumVar);
+return randomNumVar;
+}
+
+//reset feedback field
+function resetFeedback() {
+	$('h2#feedback').html('Make Your Guess!');
+}
+
+//guess counter
+function guessCounter () {
+		var guessCount = parseInt($('#count').html()) + 1;
+		$('span#count').text(guessCount++);	
+}
+
+//clear input field
+function clearInputField () {
+	$('input#userGuess').val('');
+}
+ 
+
+
+
+
+
+
+
+
+});//document ready end   
+
+
+
+
